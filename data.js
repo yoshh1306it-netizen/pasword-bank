04ba069d6af9de386ac9942165032ddb0dc11849
yoshh-owner <yoshh.1306.it@gmail.com>
2026-02-09 22:09:43 +0900

    data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+// GitHubでこのファイルを編集してパスワードを追加します
+// iconは "github", "twitter", "mail", "server", "shopping-bag" などが使えます
+
+const passwordData = [
+    {
+        service: "Google Account",
+        id: "sample@gmail.com",
+        password: "PassWord123!",
+        category: "Mail",
+        icon: "mail"
+    },
+    {
+        service: "Twitter / X",
+        id: "@my_account",
+        password: "MySecretPassword2024",
+        category: "Social",
+        icon: "twitter"
+    },
+    {
+        service: "Amazon",
+        id: "shopping-user",
+        password: "AmazonPass999",
+        category: "Shopping",
+        icon: "shopping-bag"
+    },
+    {
+        service: "AWS Console",
+        id: "root-user",
+        password: "ComplexRoutePassword#!!",
+        category: "Dev",
+        icon: "server"
+    }
+    // ↓ 新しいパスワードを追加するときは、ここより上にコピペして追加してください
+];
